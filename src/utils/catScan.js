918d35a615e59226a1a94e764d8f0648e0232fdc
Zelda <makicoryn@gmail.com>
2024-06-18 20:10:30 +0100

    cache optional

--- src/utils/catScan.js
+++ src/utils/catScan.js
@@ -321,7 +321,7 @@ async function processChannel(channel, guildId, client) {
 			if (channelUpdates[channel.name] === undefined) {
 				let hidden = false;
 
-				if (channel.permissionOverwrites.cache.size > 0) {
+				if (channel.permissionOverwrites.cache?.size > 0) {
 					const everyoneOverwrite = channel.permissionOverwrites.cache.get(
 						channel.guild.roles.everyone.id,
 					);
